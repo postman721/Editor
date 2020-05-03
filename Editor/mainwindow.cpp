9c473b424a6cb5deef678c264ef12575a9a75dd9
JJ Posti <jjposti@techtimejourney.net>
2020-05-03 13:34:11 +0300

    ++Editor
    v1 upload

---
+++ Editor/mainwindow.cpp
@@ -0,0 +1,131 @@
+
+// ++Editor is a C++ text-editor.
+//Copyright (c) 2020 JJ Posti <techtimejourney.net>
+//This is free software, and you are welcome to redistribute it under GPL Version 2, June 1991 OR Apache License v2.″
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionQuit_triggered()
+{this->close();}
+
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    QString filename=QFileDialog::getOpenFileName(this, "Open a text file");
+    QFile file(filename);
+    if(!file.open(QFile::ReadOnly | QFile::Text)){QMessageBox::warning(this, "Error", "Cannot open file.");}
+    else{
+    QTextStream stream(&file);
+    QString readme=stream.readAll();
+    ui->textEdit->setText(readme);
+    ui->statusBar->showMessage(filename);
+    file.close();}
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    QPrinter printer;
+    QPrintDialog dialog(&printer, this);
+    if(dialog.exec() != QDialog::Accepted) return;
+    ui->textEdit->print(&printer);
+}
+
+void MainWindow::on_fontComboBox_currentFontChanged(const QFont &f)
+{ui->textEdit->setFont(f);}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{ui->textEdit->zoomIn();}
+
+void MainWindow::on_pushButton_3_clicked()
+{ui->textEdit->zoomOut();}
+
+void MainWindow::on_pushButton_clicked()
+{ui->textEdit->copy();}
+
+void MainWindow::on_pushButton_7_clicked()
+{ui->textEdit->paste();}
+
+void MainWindow::on_pushButton_8_clicked()
+{ui->textEdit->undo();}
+
+void MainWindow::on_pushButton_9_clicked()
+{ui->textEdit->redo();}
+
+
+void MainWindow::on_pushButton_11_clicked()
+{   bool variable;
+    QFont font;
+    font.setItalic(true);
+    if (&variable) {ui->textEdit->setFont(font);}}
+
+void MainWindow::on_pushButton_10_clicked()
+{
+    QTextCharFormat format;
+     format.setFontWeight(QFont::Bold);
+
+    QTextCursor cursor = ui->textEdit->textCursor();
+    cursor.mergeCharFormat(format);}
+
+
+void MainWindow::on_spinBox_2_valueChanged(int arg1)
+{ui->textEdit->setFontPointSize(ui->spinBox_2->cleanText().toInt());}
+
+void MainWindow::on_pushButton_12_clicked()
+{
+    bool variable;
+    QColor color=QColorDialog::getColor(Qt::white, this);
+    if (&variable) {ui->textEdit->setTextColor(color);}
+}
+
+void MainWindow::on_pushButton_13_clicked()
+{
+    bool variable;
+    QFont font;
+    font.setUnderline(true);
+    if (&variable) {ui->textEdit->setFont(font);}
+}
+
+
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    QString filename=QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(filename);
+    if(!file.open(QFile::WriteOnly | QFile::Text)){QMessageBox::warning(this, "Error", "Cannot save the file.");}
+    else {
+    QTextStream write(&file);
+    QString content= ui->textEdit->toPlainText();
+    write << content;
+    file.flush();
+    file.close();}
+}
+
+void MainWindow::on_pushButton_14_clicked()
+{
+    QTextCharFormat format;
+     format.setFontWeight(QFont::Normal);
+
+    QTextCursor cursor = ui->textEdit->textCursor();
+    cursor.mergeCharFormat(format);
+
+    bool variable;
+    QFont font;
+    font.setUnderline(false);
+    if (&variable) {ui->textEdit->setFont(font);}
+}
---
+++ Editor/mainwindow.h
@@ -0,0 +1,68 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QKeyEvent>
+#include <QString>
+#include <QtWidgets>
+#include <QtPrintSupport>
+#include <QPrintDialog>
+#include <QPrinter>
+#include <QCoreApplication>
+#include <QProcess>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QColorDialog>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_actionQuit_triggered();
+
+    void on_fontComboBox_currentFontChanged(const QFont &f);
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_10_clicked();
+
+    void on_spinBox_2_valueChanged(int arg1);
+
+    void on_pushButton_12_clicked();
+
+    void on_pushButton_13_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_14_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
